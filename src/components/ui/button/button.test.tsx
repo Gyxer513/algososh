1c4776b8301eb553e378d7c2ea37c0598c8f87bb
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-03-12 15:51:29 +0300

    circle tests ready

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -3,32 +3,36 @@ import { Button } from "./button";
 import userEvent from "@testing-library/user-event";
 
 describe("Check Button component", () => {
-  it("should render with text", () => {
+  /* кнопки с текстом */
+  it("button with text", () => {
     const { getByRole } = render(<Button text="Text" />);
     const btn = getByRole("button");
     expect(btn).toMatchSnapshot();
   });
-  it("should render without text", () => {
+  /* кнопки без текста */
+  it("button without text", () => {
     const { getByRole } = render(<Button />);
     const btn = getByRole("button");
     expect(btn).toMatchSnapshot();
   });
+  /* заблокированной кнопки */
   it("disabled button", () => {
     const { getByRole } = render(<Button disabled />);
     const btn = getByRole("button");
     expect(btn).toMatchSnapshot();
   });
+  /* кнопки с индикацией загрузки */
   it("Button Loader is active", () => {
     const { getByRole } = render(<Button isLoader />);
     const btn = getByRole("button");
     expect(btn).toMatchSnapshot();
   });
-
+  /* Проверяем корректность вызова колбека при клике на кнопку */
   it("Button onClick", () => {
     const onClick = jest.fn();
-    render(<Button onClick={onClick} text={"Test text"} />);
+    render(<Button onClick={onClick} text={"TEST"} />);
 
-    userEvent.click(screen.getByText("Test text"));
+    userEvent.click(screen.getByText("TEST"));
     expect(onClick).toHaveBeenCalled();
   });
 });
